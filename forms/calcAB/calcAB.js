e4ba52f215fa12a326856e37022db6d22bd23174
sruedebusch <54645645+sruedebusch@users.noreply.github.com>
2020-02-23 20:50:33 -0600

    Create calcAB.js

---
+++ forms/calcAB/calcAB.js
@@ -0,0 +1,12 @@
+/*
+function calculate(n1,n2) {
+  return n1 * n2
+  }
+
+let numbersArray = [123,433,54326,2234,1919,1123,1928,3374,23,190,33874]
+let runningTotal = 1
+for (i = 0; i < numbers.length; i++)
+  runningTotal = numbers[i] * runningTotal
+
+console.log(runningTotal)
+*/
